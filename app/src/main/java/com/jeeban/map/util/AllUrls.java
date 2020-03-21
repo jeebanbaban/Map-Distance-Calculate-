554c179bc109e6218cf168338a6dfd6c329d8f71
jeeban <jeeban.bagdi@ingreens.in>
2020-03-21 20:41:02 +0530

    modified

--- app/src/main/java/com/jeeban/map/util/AllUrls.java
+++ app/src/main/java/com/jeeban/map/util/AllUrls.java
@@ -4,6 +4,5 @@ public class AllUrls {
     public static final String BASE_URL = "http://142.93.206.95:3000";
 
     public static final String GET_LOCATIONS_OR_POST_DIST_URL = "/geofence";
-    //https://maps.googleapis.com/maps/api/distancematrix/json?origins=BC|Seattle&destinations=BC&key=YOUR_API_KEY
     public static final String GET_DISTANCE_URL = "https://maps.googleapis.com/maps/api/distancematrix/json";
 }
